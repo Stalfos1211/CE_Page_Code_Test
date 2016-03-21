7c0ed3cf52a4e45b9c2f14ee05342fe86b552791
stalfos1211 <stalfos1211@gmail.com>
2016-03-20 19:19:45 -0500

    eval still needs work

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,3 +1,11 @@
+
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = 'url'; 
+
+var panelControls = '<div id=\"test-code-panel\">\r\n    \t\t<div id=\"codemirror-container\"><\/div>\r\n    \t\t<button id=\"run\">run<\/button>\r\n        <\/div>';
+
+
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
@@ -8,6 +16,15 @@ chrome.extension.sendMessage({}, function(response) {
 		console.log("Hello. This message was sent from scripts/inject.js");
 		// ----------------------------------------------------------
 
+		// Inject panel controls to body of webpage
+		document.body.innerHTML += panelControls;
+
+		var customScripts = '<script>var editorSelector = document.getElementById(\'codemirror-container\');\r\n\r\nvar editor = CodeMirror(editorSelector, {\r\n\tvalue: \"function myScript(){return 100;}\\n\",\r\n  \tmode:  \"javascript\",\r\n  \ttheme: \'ambiance\',\r\n  \tstylesheet: \"css\/CodeMirror.css\",\r\n    lineNumbers: true\r\n});'
+							+ ' $(\'#run\').click(()=>{eval(editor.getValue())}); <\/script>'; 
+		$('body').append(customScripts);
+		
+
 	}
 	}, 10);
+
 });
